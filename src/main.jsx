7a93b6e95ba4eda6f281b6fa5a4ea217b6b7156e
Sergei <Sergei.stolpovskikh@gmail.com>
2024-08-11 23:15:58 +0300

    first

---
+++ src/main.jsx
@@ -0,0 +1,60 @@
+import { createRoot } from 'react-dom/client';
+import { TermList } from './components/TermList/TermList';
+import './main.css';
+
+function saveTermList(terms) {
+	localStorage.setItem('termList', JSON.stringify(terms));
+}
+
+function restoreTermList() {
+	const rawTermList = localStorage.getItem('termList');
+
+	if (!rawTermList) {
+		return [];
+	}
+
+	return JSON.parse(rawTermList);
+}
+
+let terms = restoreTermList();
+
+function syncTermList() {
+	saveTermList(terms);
+	reactRoot.render(<TermList terms={terms} onDelete={deleteItem} />);
+}
+
+function deleteItem(id) {
+	terms = terms.filter(term => term.id !== id);
+
+	syncTermList();
+}
+
+function addTerm(title, description) {
+	terms.push({ id: Date.now(), title, description });
+
+	terms.sort((term1, term2) => (term1.title < term2.title ? -1 : 1));
+
+	syncTermList();
+}
+
+const descriptionList = document.getElementById('description-list');
+
+const reactRoot = createRoot(descriptionList);
+
+syncTermList();
+
+const form = document.getElementById('add-description');
+
+form.addEventListener('submit', event => {
+	// Отменяем поведение по умолчанию
+	event.preventDefault();
+
+	// Получаем значения полей формы
+	const title = form.elements['title'].value;
+	const description = form.elements['description'].value;
+
+	// Сбрасываем форму
+	form.reset();
+
+	addTerm(title, description);
+});
