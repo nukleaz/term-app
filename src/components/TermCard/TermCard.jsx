727eb4015a61b0bf85698579e0fa582b36850cfc
Sergei <Sergei.stolpovskikh@gmail.com>
2024-08-14 00:35:29 +0300

    TermCard and CSS changes

--- src/components/TermCard/TermCard.jsx
+++ src/components/TermCard/TermCard.jsx
@@ -1,16 +1,23 @@
 import './TermCard.css';
 
-export const TermCard = ({title, description, onDelete, id}) => {
-const handleDeleteClick = () => {
-	onDelete(id);
-}
+export const TermCard = ({ title, description, onDelete, id }) => {
+	const handleDeleteClick = () => {
+		onDelete(id);
+	};
 
 	return (
 		<div className='term-card'>
-		<h2 className='term-card__title '>{title}</h2>
-		{ description &&
-		(<p className='term-card__description'>{description}</p>)}
-		<button type="button" className="term-card__delete" onClick={handleDeleteClick}>Удалить</button>
+			<div className='text-wrap'>
+				<h2 className='term-card__title '>{title}</h2>
+				{description && <p className='term-card__description'>{description}</p>}
+			</div>
+			<button
+				type='button'
+				className='term-card__delete'
+				onClick={handleDeleteClick}
+			>
+				Удалить
+			</button>
 		</div>
-	)
-}
+	);
+};
--- src/main.jsx
+++ src/main.jsx
@@ -46,14 +46,11 @@ syncTermList();
 const form = document.getElementById('add-description');
 
 form.addEventListener('submit', event => {
-	// Отменяем поведение по умолчанию
 	event.preventDefault();
 
-	// Получаем значения полей формы
 	const title = form.elements['title'].value;
 	const description = form.elements['description'].value;
 
-	// Сбрасываем форму
 	form.reset();
 
 	addTerm(title, description);
